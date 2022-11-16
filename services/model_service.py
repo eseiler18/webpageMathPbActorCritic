9e44a0b68411f70eda1f9bd63eabda61544c4832
eseiler <emilienseiler18@gmail.com>
2022-11-16 09:17:56 +0100

    fix tokenizer

--- services/model_service.py
+++ services/model_service.py
@@ -10,8 +10,8 @@ class ModelService():
         self.output = []
 
     def forward_model(self, problem):
-        print(problem)
-        output = self.model.generate(problem, max_length=50)
+        input_ids = self.tokenizer(problem)
+        output = self.model.generate(input_ids, max_length=50)
         output = self.tokenizer.decode(output[0], skip_special_tokens=True)
         self.output.append([problem, output])
         return output
