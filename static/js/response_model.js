5903632b7e55d4c14af6751630051ea62a16c7b8
eseiler <emilienseiler18@gmail.com>
2022-12-03 20:13:06 +0100

    fix

--- static/js/response_model.js
+++ static/js/response_model.js
@@ -3,35 +3,34 @@ $("#active_train").on('change', function(e) {
   e.preventDefault();
   reload_model_selection();
 });
-  
+
 // action on checked test dataset
 $("#active_test").on('change', function(e) {
   e.preventDefault();
   reload_model_selection();
 });
 
-// load data from the checked dataset  
+// load data from the checked dataset
 function reload_model_selection(){
   var active_train = $("#active_train").is(":checked");
   var active_test = $("#active_test").is(":checked");
-  axios.post('/data_selection', {"active_train": active_train, "active_test": active_test} )
-          .then(function (response) {
-            var select_data = $("#select_data");
-            var options =  "<option value='-1' hidden>Choose a Math problem</option>" ;
-            for (let [i, opt] of Object.entries(response.data["math_pb"])){
-              options += "<option value='" + response.data["label"][i] + "'>" +  opt + "</option>";
-            }
-            select_data.html(options);
-          })
-          .catch(function (error) {
-            console.log(error);
-          });
+  axios.post('/data_selection', {"active_train": active_train, "active_test": active_test}).then(function (response) {
+    var select_data = $("#select_data");
+    var options =  "<option value='-1' hidden>Choose a Math problem</option>";
+    for (let [i, opt] of Object.entries(response.data["math_pb"])){
+      options += "<option value='" + response.data["label"][i] + "'>" +  opt + "</option>";
+    }
+    select_data.html(options);
+    })
+  .catch(function (error) {
+    console.log(error);
+  });
 }
 
 
 // action select data on the select bar
 // 1) send the label of the select data to the server
-// 2) display the select data 
+// 2) display the select data
 $("#select_data").on('change', function(e){
   display_data = $("#display_data");
   var select_data = $("#select_data").val();
