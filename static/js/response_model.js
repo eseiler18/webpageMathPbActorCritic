bcd2eae82b800f881e09797ae174328a345de2d0
eseiler <emilienseiler18@gmail.com>
2022-12-03 19:57:43 +0100

    fix

--- static/js/response_model.js
+++ static/js/response_model.js
@@ -1,8 +1,67 @@
+// action on checked train datset
+$("#active_train").on('change', function(e) {
+  e.preventDefault();
+  reload_model_selection();
+});
+  
+// action on checked test dataset
+$("#active_test").on('change', function(e) {
+  e.preventDefault();
+  reload_model_selection();
+});
+
+
+// load data from the checked dataset  
+function reload_model_selection(){
+  var active_train = $("#active_train").is(":checked");
+  var active_test = $("#active_test").is(":checked");
+  axios.post('/data_selection', {"active_train": active_train, "active_test": active_test} )
+          .then(function (response) {
+            var select_data = $("#select_data");
+            var options =  "<option value='-1' hidden>Choose a Math problem</option>" ;
+            for (let [i, opt] of Object.entries(response.data["math_pb"])){
+              options += "<option value='" + response.data["label"][i] + "'>" +  opt + "</option>";
+            }
+            select_data.html(options);
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+}
+
+// action select data on the select bar
+// 1) send the label of the select data to the server
+// 2) display the select data 
+$("#select_data").on('change', function(e){
+  display_data = $("#display_data");
+  var select_data = $("#select_data").val();
+  if (select_data != -1){
+    $("#response_model").html("")
+    $("#ask_answer").html("")
+    $("#answer_validity").html("")
+    $("#critic").html("")
+    $("#active_critic").html("")
+    $("#critic_response").html("")
+    axios.post('/display_data', {"display_data": select_data})
+      .then(function (response) {
+        console.log(response);
+        var content = " <strong> " + response.data["problem_select"] + "</strong>"
+        display_data.html(content)
+      })
+  }
+});
+
+// action on the solve button
+// we want:
+// 1) to send the data to the server
+// 2) get the response of the server (the response of the actor model fisrt turn)
+// 3) ask if the response is correct
 $("#action_select_data").on('click', function(e) {
     e.preventDefault();
-    // recup la selection
+    // get the select data label
     var select_data = $("#select_data").val();
     if (select_data != -1){
+        // send to server
         axios.post('/actor_first_turn', {"select_data": select_data})
           .then(function (response) {
             console.log(response);
@@ -41,82 +100,49 @@ $("#action_select_data").on('click', function(e) {
     }
   });
 
-function reload_model_selection(){
-  var active_train = $("#active_train").is(":checked");
-  var active_test = $("#active_test").is(":checked");
-  axios.post('/data_selection', {"active_train": active_train, "active_test": active_test} )
-          .then(function (response) {
-            var select_data = $("#select_data");
-            var options =  "<option value='-1' hidden>Choose a Math problem</option>" ;
-            for (let [i, opt] of Object.entries(response.data["math_pb"])){
-              options += "<option value='" + response.data["label"][i] + "'>" +  opt + "</option>";
-            }
-            select_data.html(options);
-          })
-          .catch(function (error) {
-            console.log(error);
-          });
-}
-
-  $("#active_train").on('change', function(e) {
-    e.preventDefault();
-    reload_model_selection();
-  });
-    
-
-  $("#active_test").on('change', function(e) {
-    e.preventDefault();
-    reload_model_selection();
-  });
-
-  $("#select_data").on('change', function(e){
-    display_data = $("#display_data");
-    var select_data = $("#select_data").val();
-    if (select_data != -1){
-      $("#response_model").html("")
-      $("#ask_answer").html("")
-      $("#answer_validity").html("")
-      $("#critic").html("")
-      $("#active_critic").html("")
-      $("#critic_response").html("")
-      axios.post('/display_data', {"display_data": select_data})
-        .then(function (response) {
-          console.log(response);
-          var content = " <strong> " + response.data["problem_select"] + "</strong>"
-          display_data.html(content)
-        })
-      }
-  });
-
-// answer button
-  function reload_answer_validity(){
-    var selected_value = $("input[name='options-outlined']:checked").val();
-    var answer_validity = $("#answer_validity");
-    var content = ""
-    if (selected_value == "good") {
-        content = "<strong> Nice the anwser is correct </strong>";
-    } else if(selected_value == "wrong"){
-        content += "<strong> Do you want to be the critic or rely on the automatic critic ? </strong> &ensp;&ensp;&ensp;&ensp; ";
-        // content += "<form id='critic_type'>"
-        content += "<div class='form-check form-check-inline'>";
-        content += "<input class='form-check-input' type='radio' name='critic_type' id='manual_critic' value='manual'>";
-        content += "<label class='form-check-label' for='critic_type'> Manual critic </label></div>";
-        content += "<div class='form-check form-check-inline'>";
-        content += "<input class='form-check-input' type='radio' name='critic_type' id='automatic_critic' value='automatic'>";
-        content += "<label class='form-check-label' for='critic_type'> Automatic critic </label></div>";
-        content += "<div class='form-check form-check-inline'>";
-        content += "<input class='form-check-input' type='radio' name='critic_type' id='oracle_critic' value='oracle'>";
-        content += "<label class='form-check-label' for='critic_type'> Oracle critic </label></div>";
-    }
-    answer_validity.html(content);
-}
 
 
+
+// action on the radio for answer validity
 $('#ask_answer').change(function(e){
   e.preventDefault();
   reload_answer_validity()
 });
+  
+function reload_answer_validity(){
+  var selected_value = $("input[name='options-outlined']:checked").val();
+  var answer_validity = $("#answer_validity");
+  var content = ""
+  // good answer
+  if (selected_value == "good") {
+      content = "<strong> Nice the anwser is correct </strong>";
+  // wrong answer ask for critic
+  } else if(selected_value == "wrong"){
+      content += "<strong> Do you want to be the critic or rely on the automatic critic ? </strong> &ensp;&ensp;&ensp;&ensp; ";
+      content += "<div class='form-check form-check-inline'>";
+      content += "<input class='form-check-input' type='radio' name='critic_type' id='manual_critic' value='manual'>";
+      content += "<label class='form-check-label' for='critic_type'> Manual critic </label></div>";
+      content += "<div class='form-check form-check-inline'>";
+      content += "<input class='form-check-input' type='radio' name='critic_type' id='automatic_critic' value='automatic'>";
+      content += "<label class='form-check-label' for='critic_type'> Automatic critic </label></div>";
+      content += "<div class='form-check form-check-inline'>";
+      content += "<input class='form-check-input' type='radio' name='critic_type' id='oracle_critic' value='oracle'>";
+      content += "<label class='form-check-label' for='critic_type'> Oracle critic </label></div>";
+  }
+  answer_validity.html(content);
+}
 
+
+// action on radio for critic selection
+$('#answer_validity').change(function(e){
+  e.preventDefault();
+  generate_critic()
+});
+
+// generate hint
+// if manual -> ask for the hint
+// if automatic -> call critic model on the server
+// if oracle -> genrate oracle hint on the server
 function generate_critic(){
   var selected_value = $("input[name='critic_type']:checked").val();
   $("#critic_response").html("")
@@ -124,41 +150,45 @@ function generate_critic(){
   var active_critic = $("#active_critic");
   var content = "";
   active_critic.html("<button class='btn btn-primary' id='action_resolve'>Resolve</button>")
+  // manual
   if (selected_value == "manual") {
       content += "<form id='hint'>";
       content += "<label for='hint_input' class='form-label'> Give an hint do the model</label>";
       content += "<input class='form-control' id='hint_input' placeholder='Follow hint template' autocomplete='off'></form>";
-  } 
+  }
+  // automatic
   else if(selected_value == "automatic"){
-      axios.post('/call_critic', {"critic_mode": selected_value}).then(function (response) {
-        content += "The critic model generate the hint : <strong> " + response.data["output"] + "</strong> </br>";
-        critic.html(content);
-      })
+    // perform automatic critic on the server
+    axios.post('/call_critic', {"critic_mode": selected_value}).then(function (response) {
+      content += "The critic model generate the hint : <strong> " + response.data["output"] + "</strong> </br>";
+      critic.html(content);
+    })
     .catch(function (error) {
       console.log(error);
     });
   }
+  // oracle 
   else if(selected_value == "oracle"){
+    // perform oracle critic on the server
     axios.post('/call_critic', {"critic_mode": selected_value}).then(function (response) {
       content += "Based on the true linear equation <strong>"  + response.data["true_linear_formula"] +"</strong> </br>"
       content += "The oracle hint is : <strong> " + response.data["output"] + "</strong> </br>";
       critic.html(content);
     })
-  .catch(function (error) {
-    console.log(error);
-  });
-}
+    .catch(function (error) {
+      console.log(error);
+    });
+  }
   critic.html(content);
 }
 
-$('#answer_validity').change(function(e){
-  e.preventDefault();
-  generate_critic()
-});
-
+// action on the resolve button
+// we want:
+// 1) to send the data to the server
+// 2) get the response of the server (the response of the actor model second turn)
 $("#active_critic").on('click', function(e) {
   e.preventDefault();
-  // recup la critic mode
+  // get critic type
   var critic_mode = $("input[name='critic_type']:checked").val();
   if (critic_mode == "manual") {
     var hint_input = $("#hint_input").val();
@@ -166,20 +196,19 @@ $("#active_critic").on('click', function(e) {
   else{
     var hint_input = "nan"
   }
-  //div_response_model.html(content)
   axios.post('/actor_second_turn', {"critic_mode": critic_mode, "hint_input": hint_input}).then(function (response) {
-          // disp response of the model
-          var critic_response = $("#critic_response");
-          var content = "The model with help of the critic generate : "
-          for (let [i, opt] of Object.entries(response.data["output"])){
-            if (i>0){
-              content += "&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp; <strong>" + opt + "</strong> <br>"
-            }
-            else{
-              content += "<strong>" + opt + "</strong> <br>"
-            }
-          }
-          critic_response.html(content);
+    // disp response of the model
+    var critic_response = $("#critic_response");
+    var content = "The model with help of the critic generate : "
+    for (let [i, opt] of Object.entries(response.data["output"])){
+      if (i>0){
+        content += "&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp; <strong>" + opt + "</strong> <br>"
+      }
+      else{
+        content += "<strong>" + opt + "</strong> <br>"
+      }
+    }
+    critic_response.html(content);
   })
   .catch(function (error) {
   console.log(error);
